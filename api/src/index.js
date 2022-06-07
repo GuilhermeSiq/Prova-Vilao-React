da69ddad4227ddc9cf0aca4cdf24530405f37e97
Guilherme Siqueira <guilellosiq@gmail.com>
2022-06-07 17:39:13 -0300

    teste

---
+++ api/src/index.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+
+import vilaocontroller from './controller/vilaocontroller.js';
+import express from 'express';
+import cors from 'cors';
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+server.use(vilaocontroller);
+
+
+
+server.listen(process.env.PORT, () => console.log(`API CONECTADA NA PORTA $ {process.env.PORT}`))
---
+++ api/src/repository/vilaorepository.js
@@ -0,0 +1,6 @@
+import {con} from '../controller/connection.js';
+
+export async function vilao(vilao){
+    const comando =
+    `INSERT INTO TB_FILME`
+}
